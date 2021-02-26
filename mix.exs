c251ca6248bc3a2b2e17dd7701842923ffd409f5
RenanArques <renanarques4448@gmail.com>
2021-02-26 00:00:46 -0300

    Create account and deposit

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Rocketpay.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      xref: [exclude: [Bcrypt]]
     ]
   end
 
